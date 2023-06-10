362915249657e1b10d361ede1f3e4189c7a26484
BryAM018 <bjam1315074052@gmail.com>
2023-06-09 22:05:16 -0500

    errror:/home/bryan/Documents/AplicacionesWeb2-2/2Practica1/node_modules/express-validator/src/middlewares/exact.js:28     const locations = opts?.locations || ['body', 'params', 'query'];                            ^
    SyntaxError: Unexpected token .
        at Module._compile (internal/modules/cjs/loader.js:723:23)
        at Object.Module._extensions..js (internal/modules/cjs/loader.js:789:10)
        at Module.load (internal/modules/cjs/loader.js:653:32)
        at tryModuleLoad (internal/modules/cjs/loader.js:593:12)
        at Function.Module._load (internal/modules/cjs/loader.js:585:3)
        at Module.require (internal/modules/cjs/loader.js:692:17)
        at require (internal/modules/cjs/helpers.js:25:18)
        at Object.<anonymous> (/home/bryan/Documents/AplicacionesWeb2-2/2Practica1/node_modules/express-validator/src/index.js:14:14)
        at Module._compile (internal/modules/cjs/loader.js:778:30)
        at Object.Module._extensions..js (internal/modules/cjs/loader.js:789:10)
    [nodemon] app crashed - waiting for file changes before starting...

--- 2Practica1/index.js
+++ 2Practica1/index.js
@@ -1,45 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const { dbConnection } = require('./database/config');
+require('dotenv').config();
+const Server  = require('./server');
 
-class Server {
-  constructor() {
-    this.app = express();
-    this.port = process.env.PORT || 2500;
 
-    this.paths = {
-      products: '/api/products',
-      categories: '/api/categories',
-      tipoGas: '/api/tipo-gas',
-    };
+const server = new Server();
 
-    this.connectDB();
-    this.middlewares();
-    this.routes();
-  }
 
-  async connectDB() {
-    await dbConnection();
-  }
-
-  middlewares() {
-    this.app.use(cors());
-    this.app.use(express.json());
-    this.app.use(express.static('public'));
-    this.app.use('/uploads/', express.static('uploads'));
-  }
-
-  routes() {
-    this.app.use(this.paths.products, require('./routes/products'));
-    this.app.use(this.paths.categories, require('./routes/categories'));
-    this.app.use(this.paths.tipoGas, require('./routes/tpGasolina'));
-  }
-
-  listen() {
-    this.app.listen(this.port, () => {
-      console.log(`Servidor ejecutándose en puerto ${this.port}`);
-    });
-  }
-}
-
-module.exports = Server;
+server.listen();
---
+++ 2Practica1/models/index.js
@@ -0,0 +1,10 @@
+const Parqueo =  require('./parqueo');
+const Espacio =  require('./espacio');
+const Vehiculo =  require('./vehiculo');
+
+
+module.exports ={
+    Parqueo,
+    Espacio,
+    Vehiculo,
+}
